d9d8becfff62b0e1b60e1b30e2770a911bce69b7
anumulaphanikumar <phanikumar.anumula@my.jcu.edu.au>
2016-12-14 14:15:01 +0800

    security_check

---
+++ pract_4/security_check.py
@@ -0,0 +1,7 @@
+
+usernames = ['phani', 'sudheel', 'abhi', 'WhatSup', 'NicolEye', 'swei45''BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+name = input("enter a name:")
+if name in usernames:
+    print("acess granted")
+else:
+    print("acess denied")
