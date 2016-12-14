ea324d43f959bef0d5f2d8cff931407d6d7dbee8
anumulaphanikumar <phanikumar.anumula@my.jcu.edu.au>
2016-12-14 14:13:54 +0800

    numbers

---
+++ pract_4/numbers.py
@@ -0,0 +1,7 @@
+
+number = [5 , 90 , 1 , 2 , 3]
+print("the first number is {} ".format(number[0]))
+print("the last number is {} ".format(number[-1]))
+print("the small number is {} ".format(min(number)))
+print("the big number is {} ".format(max(number)))
+print("the average is {} ".format(sum(number)/len(number)))
